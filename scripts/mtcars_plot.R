b2af7c5dcfeb598e2d3250fecc99439f608fe3a8
Sckramr <kramers4@vcu.edu>
2023-01-25 11:16:18 -0500

    changed the theme to theme_ipsum

--- scripts/mtcars_plot.R
+++ scripts/mtcars_plot.R
@@ -1,6 +1,7 @@
 #mtcars plots
 library(dplyr)
 library(ggplot2)
+
 #plot
 
 
@@ -8,5 +9,5 @@ library(ggplot2)
 mtcars  %>%
   ggplot(mapping = aes(x= drat, y= hp))+ geom_point(alpha= 0.9) + facet_wrap(~vs) + 
  ylab("Horse Power") + xlab("Rear axle ratio")
-+ theme_minimal()
++ theme_ipsum()
 
